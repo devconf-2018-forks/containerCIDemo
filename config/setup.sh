2693e676b62f090d1b66244eced328e40e885563
Scott Hebert <shebert@redhat.com>
2018-01-05 14:23:36 -0500

    component builds

---
+++ config/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+oc create -f component1/component1-buildconfig-template.yaml
+oc new-app component1-builder
+oc create -f component2/component2-buildconfig-template.yaml
+oc new-app component2-builder
